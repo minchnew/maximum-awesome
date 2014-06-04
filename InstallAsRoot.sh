4e25b874b8de98bc4337d3b3f990fd1a9921746c
Min Chen <minchnew@gmail.com>
2014-06-04 19:00:32 +0800

    Major changes for JavaScript development on Linux.

---
+++ InstallAsRoot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+apt-get -y install exuberant-ctags cscope tmux
+# apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
+# git clone https://github.com/ggreer/the_silver_searcher.git; cd the_silver_searcher; ./build.sh; make install
+# npm install -g jshint
